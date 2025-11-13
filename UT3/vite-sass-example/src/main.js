4cd1fd12b844ebf4429dd2b9d55ba4e09bb1054e
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2025-11-13 10:47:43 +0100

    ejemplo sass vite

--- UT3/vite-sass-example/src/main.js
+++ UT3/vite-sass-example/src/main.js
@@ -1,1 +1,9 @@
 import "./styles/main.scss";
+
+// Pequeño script para el menú móvil
+const toggle = document.querySelector(".nav__toggle");
+const menu = document.querySelector(".nav__menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("nav__menu--open");
+});
